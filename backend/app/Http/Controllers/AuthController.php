69d0af2fa2ead6b37860a436ddae3e0be0d9dabd
musoye <oyebamijimustapha44@gmail.com>
2025-06-02 13:47:03 +0100

    feat: create user auth and auth for both students and admin

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+       public function register(Request $request)
+    {
+         $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6',
+            'role' => 'required|in:admin,student'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Error in registering the user',
+                'errors' => $validator->messages()
+            ], 422);
+        };
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        $token = $user->createToken('api-token', [$user->role])->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('api-token', [$user->role])->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        if (!$request->user()) {
+            return response()->json(['message' => 'No user is logged in'], 401);
+        }
+
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out']);
+    }
+
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\RoleMiddleware;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
+
+// Route::middleware([RoleMiddleware::class . ':admin'])->group(function () {
+//     Route::get('/admin-dashboard', function () {
+//         // Only accessible by users with role 'admin'
+//     });
+
+//     Route::get('/admin-settings', function () {
+//         // Only accessible by users with role 'admin'
+//     })->withoutMiddleware([RoleMiddleware::class . ':admin']);
+// });
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
